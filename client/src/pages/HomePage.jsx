f5eb565e1cc3325ece2827994917ea0f0ba52e36
SamuelJebaraj2425 <samuelkirubakaran2425@gmail.com>
2025-05-29 09:25:19 +0530

    home page

---
+++ client/src/pages/HomePage.jsx
@@ -0,0 +1,8 @@
+export default function HomePage() {
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen bg-gray-50">
+            <h1 className="text-4xl font-bold text-blue-700 mb-4">Welcome to the Home Page</h1>
+            <p className="text-lg text-gray-700">This is a group project done by three members.This application is used as a cms project.</p>
+        </div>
+    );
+}
